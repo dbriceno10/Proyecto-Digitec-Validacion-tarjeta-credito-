98fd215359b04999ecfd9061a4143aa95edccf1a
nombre <daniel.b100726@gmail.com>
2021-06-07 16:27:48 -0400

    first commit rama2

---
+++ src/Validator.js
@@ -0,0 +1,7 @@
+const CardValidator = (digit) => {
+    let arrayCard = []
+    arrayCard = digit.split("")
+    console.log(arrayCard)
+}
+
+CardValidator("123456789")
---
+++ src/index.js
@@ -0,0 +1,1 @@
+const cardNumber = document.getElementById("ingresar").value.toString()
